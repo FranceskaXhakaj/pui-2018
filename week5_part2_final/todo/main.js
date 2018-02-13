50a886ed7a2cdee9473be4ee97cd3c44d34a15b9
Franceska Xhakaj <francesx@F15PHD21.HCII.CS.CMU.EDU>
2018-02-13 11:45:36 -0500

    adding to do

---
+++ week5_part2_final/todo/main.js
@@ -0,0 +1,29 @@
+function addNewList() {
+    alert('hello world!');
+}
+
+function addListItem() {
+    console.log('hello world');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item");
+    var newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+	    // remove li element (item) from ol element (item.parentNode)
+ 	    item.parentNode.removeChild(item);
+}
+
+
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
+}
+
