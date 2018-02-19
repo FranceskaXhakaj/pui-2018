218df920c289f7571d352128867dadd62fa4d9bb
Franceska Xhakaj <francesx@F15PHD21.HCII.CS.CMU.EDU>
2018-02-19 10:55:55 -0500

    Final version of lab

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -27,3 +27,42 @@ function completeListItem(item) {
     }
 }
 
+// $(document).ready(function(){
+//     $("#add-item").click(function() { // bind handler for click event
+//         var list = $("#grocery-list"); // get the ol list by id
+//         var itemInput = $("#new-list-item"); // get the new item input
+//       // append the input value within an li element
+//         list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>") 
+//     });
+
+//     $(".delete-item").click(function() {
+//         console.log("test")
+//         $(this).parent().remove();
+//     });
+
+// });
+
+
+$(document).on("click", ".delete-item", function() {
+    $(this).parent().remove();
+});
+
+$(document).on("click", "#add-item", function() {
+    var list = $("#grocery-list");
+    var itemInput = $("#new-list-item");
+    list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>");
+});
+
+$(document).on("click", ".complete-item", function() {
+
+    if ($(this).is(':checked')) { // true if the input checkbox is checked
+        // console.log("entered if");
+        $(this).parent().css('text-decoration', 'line-through');
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        // console.log("entered else");
+        $(this).parent().css('text-decoration', 'none');
+        // in css, this would be: "text-decoration: none"
+    }
+});
+
