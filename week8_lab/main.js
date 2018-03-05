8c70f4de43580384642931e0dd16ddd72e0de2c4
Franceska Xhakaj <francesx@F15PHD21.HCII.CS.CMU.EDU>
2018-03-05 17:03:10 -0500

    initial version of week 8

---
+++ week8_lab/main.js
@@ -0,0 +1,73 @@
+/*** Object Constructors ***/
+function Cat(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image = "cat.jpg";
+  this.type = "Cat";
+}
+
+function Dog(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image = "dog.jpg"
+  this.type = "Dog";
+}
+
+function Bird(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image = "bird.jpg"
+  this.type = "Bird";
+}
+
+/*** Global Variables ***/
+var animals = [new Cat(), new Dog(), new Bird()];
+var names = ["Toothless", "Marshmallow", "Momo", "Coco", "Ollie", "Oscar", "Bella", "Ruby", "Apples"];
+
+/*** Functions ***/
+// get a random index for an array from 0 to maxIndex (not inclusive)
+function getRandomIndex(maxIndex) {
+  return Math.floor(Math.random() * maxIndex);
+}
+
+// generates either a Cat, Dog, or Bird with a random name and random age
+function generateRandomAnimal() {
+  var randomIdx = getRandomIndex(animals.length);
+  var randomAnimal = animals[randomIdx];
+
+  if (randomAnimal instanceof Cat) 
+  {
+    return new Cat(generateRandomName(), generateRandomAge());
+  } 
+  else if (randomAnimal instanceof Dog) 
+  {
+    return new Dog(generateRandomName(), generateRandomAge());
+  } 
+  else if (randomAnimal instanceof Bird) 
+  {
+    return new Bird(generateRandomName(), generateRandomAge());
+  }
+}
+
+// generates a random name from list of names
+function generateRandomName() {
+  var randomIdx = getRandomIndex(names.length);
+  return names[randomIdx];
+}
+
+// generates a random age from 0 to 5
+function generateRandomAge() {
+  var randomIdx = getRandomIndex(5);
+  return randomIdx;
+}
+
+/*** Document Load ****/
+$(document).ready(function() {
+
+  // generate a random animal when the document opens
+  var animal = generateRandomAnimal();
+  // update the page based on the animal properties
+  $("#animal-properties").text(animal.name + "  " + animal.age + "years old");
+  $("#animal-img").attr("src", animal.image);
+
+});
